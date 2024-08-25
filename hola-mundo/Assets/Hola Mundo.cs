e42086c504e8fe2b2aa3e5d45ec27e4c5bcafc48
GulliFerWD <clxl22@hotmail.com>
2024-08-25 12:54:56 -0600

    hola mundo

---
+++ hola-mundo/Assets/Hola Mundo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hola Mundo desde el curso Diseño de Juegos II");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
